6d8d663a11efb2e41722ca10b6553d1dada37286
A6u7H <danji.krivenkov@gmail.com>
2023-01-03 00:14:04 +0400

    prep

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -1,6 +1,7 @@
+import argparse
 import logging
-import pickle
 import json
+import os
 import configparser
 import pandas as pd
 import numpy as np
@@ -43,15 +44,31 @@ class BCCPreprocessor:
 
 
     def save_data(self, data: pd.DataFrame, mode: str="train"):
+        experiments_dir = self.config["PREPROCESSOR"]["experiments_dir"]
+        if not os.path.exists(experiments_dir):
+            os.mkdir(experiments_dir)
+
         if mode == "train":
-            logger.debug(f"Try to save data to: {self.config['PREPROCESSOR']['train_preprocessed']}")
-            data.to_csv(self.config["PREPROCESSOR"]["train_preprocessed"], index=False)
+            train_split_path = os.path.join(
+                experiments_dir, 
+                "BBC_News_Train_Split.csv"
+            )
+            logger.debug(f"Try to save data to: {train_split_path}")
+            data.to_csv(train_split_path, index=False)
         elif mode == "val":
-            logger.debug(f"Try to save data to: {self.config['PREPROCESSOR']['val_preprocessed']}")
-            data.to_csv(self.config["PREPROCESSOR"]["val_preprocessed"], index=False)
+            val_split_path = os.path.join(
+                experiments_dir, 
+                "BBC_News_Val_Split.csv"
+            )
+            logger.debug(f"Try to save data to: {val_split_path}")
+            data.to_csv(val_split_path, index=False)
         elif mode == "test":
-            logger.debug(f"Try to save data to: {self.config['PREPROCESSOR']['test_preprocessed']}")
-            data.to_csv(self.config["PREPROCESSOR"]["test_preprocessed"], index=False)
+            test_split_path = os.path.join(
+                experiments_dir, 
+                "BBC_News_Test_Split.csv"
+            )
+            logger.debug(f"Try to save data to: {test_split_path}")
+            data.to_csv(test_split_path, index=False)
         else:
             raise ValueError(f"Current mode {mode} not exist")
 
@@ -59,10 +76,18 @@ class BCCPreprocessor:
         with open(self.config["PREPROCESSOR"]["target_mapping"], "w") as fp:
             json.dump(self.category2id,fp)
 
+def parse_args():
+    default_path = "/home/dkrivenkov/program/mipt_mle/news_classification/configs/train_config.ini"
+    
+    parser=argparse.ArgumentParser(description="predict script")
+    parser.add_argument("--config_path", type=str, default=default_path)
+    args=parser.parse_args()
+    return args
+
 
 if __name__ == "__main__":
-    config_path = "/home/dkrivenkov/program/mipt_mle/news_classification/configs/train_config.ini"
-    preprocessor = BCCPreprocessor(config_path)
+    args = parse_args()
+    preprocessor = BCCPreprocessor(args.config_path)
 
     bbc_train_data = preprocessor.load_train_data()
     bbc_test_data = preprocessor.load_test_data()
